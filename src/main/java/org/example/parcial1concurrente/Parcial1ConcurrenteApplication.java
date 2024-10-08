b7ac03bc6c4c97883eb4aac56dcf122ba90ea92f
marcos <marcgabe@myuax.com>
2024-10-08 13:19:10 +0200

    Initial commit

---
+++ src/main/java/org/example/parcial1concurrente/Parcial1ConcurrenteApplication.java
@@ -0,0 +1,13 @@
+package org.example.parcial1concurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial1ConcurrenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial1ConcurrenteApplication.class, args);
+	}
+
+}
