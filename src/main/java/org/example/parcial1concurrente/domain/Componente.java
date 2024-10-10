3c96a42eac5b9a5c16bbf38ced67e00790dfe0dd
marcos <marcgabe@myuax.com>
2024-10-10 19:59:25 +0200

    Update Componente.java
    Adiciones insignificantes.

--- src/main/java/org/example/parcial1concurrente/domain/Componente.java
+++ src/main/java/org/example/parcial1concurrente/domain/Componente.java
@@ -1,16 +1,18 @@
 package org.example.parcial1concurrente.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Componente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nombre;
-
-    // Getters y setters
 }
